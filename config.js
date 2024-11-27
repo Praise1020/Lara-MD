fc2beae270a0a303cd6b04b123e84a4767f7966d
Praise1020 <robertspraise581@gmail.com>
2024-11-27 18:42:32 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //paste your session_id
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
+SESSION_ID: process.env.SESSION_ID || "G65SBIKJ#A49nfO00ub_NYpDZ-F1gAXkxcBF3UTNeJiBC117EplM", //paste your session_id
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false", 
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
